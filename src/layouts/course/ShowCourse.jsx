02fd3be69925e373b207cb988f61b33079b82c8f
21SW066 <zainailqk@gmail.com>
2024-12-09 16:19:09 +0500

    Modifications was made by muqadas

--- src/layouts/course/ShowCourse.jsx
+++ src/layouts/course/ShowCourse.jsx
@@ -1,7 +1,29 @@
+import { useParams } from "react-router-dom";
 import DashboardPageCompement from "../../components/global/DashboardPage";
+import { useEffect, useState } from "react";
+import blueprint from "../../uitils/blueprint";
+import courseapi from "../../uitils/api/course";
+import { useAuth } from "../../contexts/Authentication";
+import { useHandler } from "../../contexts/Handler";
 
 export default function ShowCourse() {
-    return <DashboardPageCompement title={'specified course'}>
-        <h1>This is an specified course page</h1>
+  const { id } = useParams();
+  const {
+    credentials: { token },
+  } = useAuth();
+  const { handler } = useHandler();
+  const [course, setCourse] = useState(blueprint.course);
+
+  useEffect(() => {
+    courseapi.show(id, token, setCourse, handler);
+  }, [id]);
+
+  return (
+    <DashboardPageCompement title={"specified course"}>
+      <div>
+        <h1>{course.title}</h1>
+        <p>{course.description}</p>
+      </div>
     </DashboardPageCompement>
+  );
 }
---
+++ src/uitils/blueprint.js
@@ -0,0 +1,11 @@
+const course = Object.freeze({
+    id: '',
+    user_id: {},
+    title:'',
+    description:'',
+    image: '',
+    created_at: '',
+    updated_at: '',
+});
+
+export default {course};

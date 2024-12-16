8913dcd616eea4047ff4bb795e609601713fd65b
Aashique-Ali <aashique.ali2002@gmail.com>
2024-12-16 10:49:32 +0500

    add button and table  design

--- src/layouts/course/AddCourse.jsx
+++ src/layouts/course/AddCourse.jsx
@@ -1,45 +1,41 @@
-import DashboardPageCompement from "../../components/global/DashboardPage";
-import Form from "../../components/form/Form";
-import InputField from "../../components/form/InputField";
-import SubmitButton from "../../components/form/SubmitButton";
-import course from "../../uitils/api/course";
-import { useAuth } from "../../contexts/Authentication";
-import { useHandler } from "../../contexts/Handler";
-import { isLoading } from "../../uitils/functions/global";
+import DashboardPageCompement from "../../components/global/DashboardPage"
+import Form from "../../components/form/Form"
+import InputField from "../../components/form/InputField"
+import SubmitButton from "../../components/form/SubmitButton"
+import course from "../../uitils/api/course"
+import { useAuth } from "../../contexts/Authentication"
+import { useHandler } from "../../contexts/Handler"
+import { isLoading } from "../../uitils/functions/global"
 
 export default function AddCourse() {
-    const { credentials: { token, user } } = useAuth();
-    const { handler } = useHandler();
+  const {
+    credentials: { token, user },
+  } = useAuth()
+  const { handler } = useHandler()
 
-    function handleSubmit(data) {
-        course.store(token, data, handler);
-    }
+  function handleSubmit(data) {
+    course.store(token, data, handler)
+  }
 
-    return (
-        <DashboardPageCompement title={"add course"}>
-            <h1>Add a new course</h1>
+  return (
+    <DashboardPageCompement title={"add course"}>
+      <h1 className="text-center">Add a new course</h1>
 
-            <Form {...{ handleSubmit }}>
-                <InputField type={"text"}
-                    name={"title"}
-                    placeholder={"Course title"} />
+      <Form {...{ handleSubmit }}>
+        <InputField type={"text"} name={"title"} placeholder={"Course title"} />
 
-                <InputField
-                    type={"text"}
-                    name={"description"}
-                    placeholder={"Course description"}
-                />
+        <InputField
+          type={"text"}
+          name={"description"}
+          placeholder={"Course description"}
+        />
 
-                <InputField type={"file"} name={"image"} accept={".jpg,.jpeg.png"} />
+        <InputField type={"file"} name={"image"} accept={".jpg,.jpeg.png"} />
 
-                <InputField
-                    type={"hidden"}
-                    name={"user_id"}
-                    value={user.id}
-                />
+        <InputField type={"hidden"} name={"user_id"} value={user.id} />
 
-                <SubmitButton name={isLoading(handler, "Add Course")} />
-            </Form>
-        </DashboardPageCompement>
-    );
+        <SubmitButton name={isLoading(handler, "Add Course")} />
+      </Form>
+    </DashboardPageCompement>
+  )
 }
--- src/layouts/course/AllCourse.jsx
+++ src/layouts/course/AllCourse.jsx
@@ -1,47 +1,72 @@
-import { useEffect, useState } from "react";
-import DashboardPageCompement from "../../components/global/DashboardPage";
-import Table from '../../components/global/Table';
-import { useHandler } from "../../contexts/Handler";
-import { useAuth } from "../../contexts/Authentication";
-import course from "../../uitils/api/course";
-import { Link } from "react-router-dom";
-import { useDelete } from "../../contexts/Delete";
-import blueprint from "../../uitils/blueprint";
-import { capEach } from "../../uitils/functions/global";
+import { useEffect, useState } from "react"
+import DashboardPageCompement from "../../components/global/DashboardPage"
+import Table from "../../components/global/Table"
+import { useHandler } from "../../contexts/Handler"
+import { useAuth } from "../../contexts/Authentication"
+import course from "../../uitils/api/course"
+import { Link } from "react-router-dom"
+import { useDelete } from "../../contexts/Delete"
+import blueprint from "../../uitils/blueprint"
+import { capEach } from "../../uitils/functions/global"
+import ActionButton from "../../components/global/ActionButton"
 
 export default function AllCourses() {
-    const { handler } = useHandler();
-    const { credentials: { user, token } } = useAuth();
-    const [courses, setCourses] = useState([blueprint.course]);
-    const { destory } = useDelete();
+  const { handler } = useHandler()
+  const {
+    credentials: { user, token },
+  } = useAuth()
+  const [courses, setCourses] = useState([blueprint.course])
+  const { destory } = useDelete()
 
-    useEffect(() => {
-        course.all(token, setCourses, handler);
-    }, [handler.navigate, user]);
+  useEffect(() => {
+    course.all(token, setCourses, handler)
+  }, [handler.navigate, user])
 
-    return <DashboardPageCompement title={'all courses'}>
-        <Link to={'./add'}>Add a new course</Link>
+  return (
+    <DashboardPageCompement title={"all courses"}>
+      <Link to={"./add"}>
+        <button className=" bg-gradient-to-r from-[#21bffd] to-[#217bfe] text-white py-2 px-4 rounded-lg ">
+          Add a new course
+        </button>
+      </Link>
 
-        <h1>The all courses are below in a table form</h1>
-        <Table
-            ths={<>
-                <th>Sno.</th>
-                <th>Title</th>
-                <th>Description</th>
-                <th>Created On</th>
-                <th>Action</th>
-            </>}
-
-            tds={courses.at(0).id && courses.map((course, index) => <tr>
-                <td>{index + 1}</td>
-                <td>{course.title}</td>
-                <td>{course.description}</td>
-                <td>
-                    <Link to={'./' + course.id}>View</Link>
-                    <Link to={'./edit/' + course.id}>Edit</Link>
-                    <button onClick={() => destory('/courses', course.id, capEach(course.title + ' course'))}>Delete</button>
-                </td>
-            </tr>)}
-        />
+      <h1 className="text-center"> All Courses Table </h1>
+      <Table
+        ths={
+          <>
+            <th>Sno.</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Created On</th>
+            <th>Action</th>
+          </>
+        }
+        tds={
+          courses.at(0).id &&
+          courses.map((course, index) => (
+            <tr>
+              <td>{index + 1}</td>
+              <td>{course.title}</td>
+              <td>{course.description}</td>
+              <td>{course.created_at}</td>
+              <td>
+                <ActionButton name={"View"} route={"./" + course.id} />
+                <button
+                  onClick={() =>
+                    destory(
+                      "/courses",
+                      course.id,
+                      capEach(course.title + " course")
+                    )
+                  }
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))
+        }
+      />
     </DashboardPageCompement>
+  )
 }
